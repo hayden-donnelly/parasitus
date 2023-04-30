00abe5d46f0a139edb2b33beb070cc91f17ddfe7
hayden-donnelly <donnellyhd@outlook.com>
2023-04-29 22:59:41 -0400

    Day 1.

--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -5,7 +5,7 @@ public class FirstPersonController : MonoBehaviour
     [SerializeField] private float moveSpeed = 4.0f;
     [SerializeField] private float rotationSpeed = 5.0f;
     [SerializeField] private float aimFov = 50;
-    [SerializeField] private float readyToShootFov = 46;
+    [SerializeField] private float readyToShootFov = 55;
     [SerializeField] private float regularFov = 60;
     [SerializeField] private float timeBetweenShots = 0.7f;
     private float timeSinceLastShot = 0.0f;
@@ -14,6 +14,9 @@ public class FirstPersonController : MonoBehaviour
     [SerializeField] private Transform cameraTransform;
     [SerializeField] private Camera viewCamera;
     [SerializeField] private AudioSource gunshotSource;
+    [SerializeField] private GameObject crosshair;
+    [SerializeField] private Transform bulletOrigin;
+    [SerializeField] private GameObject nonEnemyHitEffect;
 
     [SerializeField] private AnimationCurve aimCurve;
     private float aimTime = 0.0f;
@@ -21,6 +24,8 @@ public class FirstPersonController : MonoBehaviour
     private void Start()
     {
         controller = GetComponent<CharacterController>();
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     private void Update()
@@ -29,17 +34,47 @@ public class FirstPersonController : MonoBehaviour
         float moveX = 0;
         timeSinceLastShot += Time.deltaTime;
 
+        if(viewCamera.fieldOfView <= readyToShootFov)
+        {
+            crosshair.SetActive(true);
+        }
+        else
+        {
+            crosshair.SetActive(false);
+        }
+
         if(Input.GetMouseButton(1))
         {
             aimTime += Time.deltaTime;
             float aimValue = aimCurve.Evaluate(aimTime);
             viewCamera.fieldOfView = Mathf.Lerp(regularFov, aimFov, aimValue);
-            if(viewCamera.fieldOfView >= readyToShootFov)
+            if(viewCamera.fieldOfView <= readyToShootFov)
             {
                 if(Input.GetMouseButtonDown(0))
                 {
                     if(timeSinceLastShot >= timeBetweenShots)
                     {
+                        RaycastHit hit;
+                        if(Physics.Raycast(bulletOrigin.position, bulletOrigin.forward, out hit, 20.0f))
+                        {
+                            if(hit.collider.CompareTag("Enemy Body"))
+                            {
+                                DamageEnemyFromRaycast(hit, false);
+                            }
+                            else if(hit.collider.CompareTag("Enemy Head"))
+                            {
+                                DamageEnemyFromRaycast(hit, true);
+                            }
+                            else
+                            {
+                                Debug.Log("Hit something else");
+                                Debug.Log(hit.point);
+                                GameObject effectInstance = Instantiate(nonEnemyHitEffect, hit.point, Quaternion.identity);
+                                Destroy(effectInstance, 0.5f);
+                                effectInstance.transform.forward = hit.normal;
+                            }
+                        }
+
                         gunshotSource.Play();
                         timeSinceLastShot = 0.0f;
                     }
@@ -76,5 +111,11 @@ public class FirstPersonController : MonoBehaviour
         float mouseY = Input.GetAxis("Mouse Y");
         transform.Rotate(Vector3.up, mouseX * rotationSpeed * Time.deltaTime);
         cameraTransform.Rotate(Vector3.right, -mouseY * rotationSpeed * Time.deltaTime);
+    }
+
+    private void DamageEnemyFromRaycast(RaycastHit hit, bool headshot)
+    {
+        GameObject enemy = hit.collider.transform.parent.gameObject;
+        enemy.GetComponent<EnemyBase>().TakeDamage(headshot);
     }
 }
